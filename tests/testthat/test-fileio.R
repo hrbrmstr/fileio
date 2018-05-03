9983d988cd78d852a1af2083e4086bf50850882d
boB Rudis <bob@rud.is>
2018-05-03 17:49:42 -0400

    trying one more time for tests on travis & appveyor

--- tests/testthat/test-fileio.R
+++ tests/testthat/test-fileio.R
@@ -1,9 +1,14 @@
 context("post a file")
 test_that("we can post a file", {
 
+  testthat::skip_on_cran()
+
   x <- fi_post_file(system.file("extdat", "tst.txt", package = "fileio"), expires = "1d")
 
   expect_true(x$success)
+
+  testthat::skip_on_appveyor()
+  testthat::skip_on_travis()
 
   con <- url(x$link)
   tmp <- readLines(con, warn = FALSE)
@@ -11,15 +16,19 @@ test_that("we can post a file", {
 
   expect_equal(tmp, "Hi Noam!")
 
-
 })
 
 context("post text")
 test_that("we can post text", {
 
+  testthat::skip_on_cran()
+
   x <- fi_post_text("Hi Noam!", expires = "1d")
 
   expect_true(x$success)
+
+  testthat::skip_on_appveyor()
+  testthat::skip_on_travis()
 
   con <- url(x$link)
   tmp <- readLines(con, warn = FALSE)
@@ -32,6 +41,8 @@ test_that("we can post text", {
 context("post arbitrary R data")
 test_that("we can post arbitrary R data", {
 
+  testthat::skip_on_cran()
+
   fi_post_rdata(
     list(
       mtcars = mtcars,
@@ -40,6 +51,9 @@ test_that("we can post arbitrary R data", {
     ),
     expires = "1d"
   ) -> x
+
+  testthat::skip_on_appveyor()
+  testthat::skip_on_travis()
 
   expect_true(x$success)
 
